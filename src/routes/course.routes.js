2e040a01ebb3eb21271ee0d7099257e868984b4c
narek-chakhoyan <narek.chakhoyan@volo.global>
2023-12-26 16:48:36 +0400

    added new code line to handle public

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,18 +1,18 @@
 const { Router } = require("express");
-const fs = require('fs');
+const fs = require("fs");
 const router = Router();
 
 router.get("/topCourses", async (req, res) => {
   try {
-    console.log("here")
+    console.log("here");
     fs.readFile("./public/topCourses.json", "utf8", (err, data) => {
       if (err) {
+        res.send("Cant take data");
         return;
       }
       console.log(data);
       res.send(data);
     });
-    res.send('Hello 11 World!')
   } catch (e) {
     res.status(500).json({ e });
   }

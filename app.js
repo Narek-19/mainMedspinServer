226b1d3405bf11a429eb95a0230c9c6096b4e2d7
narek-chakhoyan <narek.chakhoyan@volo.global>
2023-12-26 12:02:33 +0400

    added new port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = "https://main-medspin-server.vercel.app"
+const port = "5000"
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

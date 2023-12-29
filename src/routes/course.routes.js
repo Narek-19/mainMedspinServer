405ad0fa23c73cbd962323ccbec6c812cd14a3e2
narek-chakhoyan <narek.chakhoyan@volo.global>
2023-12-29 11:03:11 +0400

    added new path to take json data

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,18 +1,19 @@
 const { Router } = require("express");
 const fs = require("fs");
+const topCourseData = require("../../public/topCourses.json");
 const router = Router();
 
 router.get("/topCourses", async (req, res) => {
   try {
-    console.log("here");
-    fs.readFile("./public/topCourses.json", "utf8", (err, data) => {
-      if (err) {
-        res.send("Cant take data");
-        return;
-      }
-      console.log(data);
-      res.send(data);
-    });
+    // fs.readFile("./public/topCourses.json", "utf8", (err, data) => {
+    //   if (err) {
+    //     res.send("Cant take data");
+    //     return;
+    //   }
+    //   console.log(data);
+     
+    // });
+    res.send(topCourseData);
   } catch (e) {
     res.status(500).json({ e });
   }

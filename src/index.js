954d6527b7add7584760220a4ce3b138fbe89256
Narek-19 <narekchakhoyan97@gmail.com>
2024-01-07 21:05:17 +0400

    added path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || "5000"
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile('Hello World!',{root:path.join(__dirname,"public")});
 })
 
 app.use("/api/course", require("./routes/course.routes"));
